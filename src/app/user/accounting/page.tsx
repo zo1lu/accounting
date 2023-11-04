bd6f427a0916d4b3bf2d240b8fad10dab30b1a25
Zoey Lu <luchin601@gmail.com>
2023-11-04 17:09:26 +0800

    Fix accounting page auth redirect

--- src/app/user/accounting/page.tsx
+++ src/app/user/accounting/page.tsx
@@ -1,20 +1,28 @@
 'use client'
-import {useEffect, useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import Form from '../../../components/Form'
 import { useRouter } from 'next/navigation'
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, onAuthStateChange } from '../../../firebase/firebase'
 import { getAuth } from 'firebase/auth';
+import { AuthContext } from '@/context/AuthContext';
+import { set } from 'firebase/database';
+
 export default function Accounting(){
     let router = useRouter()
     const [user, setUser] = useState({uid:"",email:""})
     
     useEffect(()=>{
-        const listner = onAuthStateChange(setUser)
-        return listner
-    },[])
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser({uid: user.uid, email: user.email})
+            } else {
+                router.push("../login")
+            }
+        })
+        return unsubscribe
+    }, [])
 
-    
     return(
         <>
             <div className='w-screen flex justify-center'>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,15 +3,17 @@ import AuthReducer from './AuthReducer';
 
 const init_state = {
     currentUser: null,
+    uid: null,
+    email: null
 }
 export const AuthContext = createContext(init_state);
 
-export const AuthContextProvider = ({children})=>{
-    const [state, dispatch] = useReducer(AuthReducer, init_state);
+// export const AuthContextProvider = ({children})=>{
+//     const [state, dispatch] = useReducer(AuthReducer, init_state);
 
-    return (
-        <AuthContext.Provider value={{currentUser: state.currentUser, dispatch}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+//     return (
+//         <AuthContext.Provider value={{currentUser: state.currentUser, dispatch}}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+// }
